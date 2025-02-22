a7d75db05b3311762c801073e07698298da7830f
shivanandgn033 <shivanandgn033@gmail.com>
2025-02-22 23:02:06 +0530

    update

--- designpatterns/designpatterns/dp/Composite.cs
+++ designpatterns/designpatterns/dp/Composite.cs
@@ -3,56 +3,51 @@
 using System.Collections.Generic;
 
 // Component Interface
-public interface IComponent
-{
-    string Name { get; }
-    void Display();
+public interface IComponent {
+  string Name {
+    get;
+  }
+  void Display();
 }
 
 // Leaf: File
-public class FileComponent : IComponent
-{
-    public string Name { get; }
+public class FileComponent: IComponent {
+  public string Name {
+    get;
+  }
 
-    public FileComponent(string name)
-    {
-        Name = name;
-    }
+  public FileComponent(string name) {
+    Name = name;
+  }
 
-    public void Display()
-    {
-        Console.WriteLine($"File: {Name}");
-    }
+  public void Display() {
+    Console.WriteLine($ "File: {Name}");
+  }
 }
 
 // Composite: Directory
-public class DirectoryComponent : IComponent
-{
-    public string Name { get; }
-    private List<IComponent> _children = new List<IComponent>();
+public class DirectoryComponent: IComponent {
+  public string Name {
+    get;
+  }
+  private List < IComponent > _children = new List < IComponent > ();
 
-    public DirectoryComponent(string name)
-    {
-        Name = name;
-    }
+  public DirectoryComponent(string name) {
+    Name = name;
+  }
 
-    public void Add(IComponent component)
-    {
-        _children.Add(component);
-    }
+  public void Add(IComponent component) {
+    _children.Add(component);
+  }
 
-    public void Remove(IComponent component)
-    {
-        _children.Remove(component);
-    }
+  public void Remove(IComponent component) {
+    _children.Remove(component);
+  }
 
-    public void Display()
-    {
-        Console.WriteLine($"Directory: {Name}");
-        foreach (var component in _children)
-        {
-            component.Display(); // Recursively display children
-        }
+  public void Display() {
+    Console.WriteLine($ "Directory: {Name}");
+    foreach(var component in _children) {
+      component.Display(); // Recursively display children
     }
+  }
 }
-
