01a2fe9cc55e08af879f1ee784504c9225936825
shivanandgn033 <shivanandgn033@gmail.com>
2025-02-23 20:07:48 +0530

    Update KISS

---
+++ SOLID/solidprinciples/Principle/KISS.cs
@@ -0,0 +1,60 @@
+﻿namespace solidprinciples.Principle;
+
+public class KISS
+{
+   //As you can see here, the method that uses a Switch-Case construct is much simpler and easier to understand.
+    public static string GetMonthNameUsingSwitchCase(int number)
+   {
+     switch (number)
+     {
+         case 1:
+             return "January";
+         case 2:
+             return "February";
+         case 3:
+             return "March";
+         case 4:
+             return "April";
+         case 5:
+             return "May";
+         case 6:
+             return "June";
+         case 7:
+             return "July";
+         case 8:
+             return "August";
+         case 9:
+             return "September";
+         case 10:
+             return "October";
+         case 11:
+             return "November";
+         case 12:
+             return "December";
+         default:
+             throw new InvalidOperationException();
+        }
+   }
+
+public static string GetMonthNameUsingIfElse(int number)
+{
+     if ((number < 1) || (number > 12)) throw new InvalidOperationException();
+         string[] months = {
+             "January",
+             "February",
+             "March",
+             "April",
+             "May",
+             "June",
+             "July",
+             "August",
+             "September",
+             "October",
+             "November",
+             "December"
+     };
+         return months[number - 1];
+}
+
+
+}
--- SOLID/solidprinciples/obj/Debug/net9.0/solidprinciples.AssemblyInfo.cs
+++ SOLID/solidprinciples/obj/Debug/net9.0/solidprinciples.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("solidprinciples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728b773cf313fa0ce225cb9d7f23f4447846f2a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98bf354a3bf672a2c41b4f5d730f63d67023f46a")]
 [assembly: System.Reflection.AssemblyProductAttribute("solidprinciples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("solidprinciples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
