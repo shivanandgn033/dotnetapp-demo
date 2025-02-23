98bf354a3bf672a2c41b4f5d730f63d67023f46a
shivanandgn033 <shivanandgn033@gmail.com>
2025-02-23 19:56:58 +0530

    Update DRY

---
+++ SOLID/solidprinciples/Principle/DRY.cs
@@ -0,0 +1,11 @@
+﻿namespace solidprinciples.Principle;
+
+public class DRY
+{
+         
+    public static int AddIntegers(int num1, int num2)
+     {
+            return num1 + num2;
+     }
+
+}
--- SOLID/solidprinciples/Program.cs
+++ SOLID/solidprinciples/Program.cs
@@ -32,3 +32,24 @@ Console.WriteLine($"Rectangle Area: {rect.Area()}"); // Output: 200
 // SetRectangleDimensions(sq, 20, 20); // This won't compile because sq is a Square, not a Rectangle.
  Console.WriteLine("\n..........................................................................");
 // //........................................................................
+
+//,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+//Using methods: Methods implement the DRY principle by allowing you to centralize code that will be used in multiple parts of your application. It is often easier to move the code that is repeated in multiple places into a method and call it from everywhere. Any required changes can be done at a single location in your code base.
+//Using classes and inheritance: When lines of code are spread across many classes and methods, they can be relocated to a parent class, which all of the derived classes can then inherit. This approach ensures that modifications to the code can be made in a single location rather than in each separate class that extends the base class.
+//Using interfaces: Code shared across classes without a common base class may be moved into an interface and implemented by the relevant classes. In this way, the code will be shared throughout all types, such that if you ever need to modify the code, you only have to update the interface.
+            Console.WriteLine("\n..........................................................................");
+            Console.WriteLine("\n.DRY principle example");
+            int a = 5;
+            int b = 10;
+            int x = 10;
+            int y = 20;
+            // Calling the AddIntegers method twice
+            int s1 = DRY.AddIntegers(a, b);
+            int s2 = DRY.AddIntegers(x, y);
+            Console.WriteLine($"Sum of a and b is: {s1}, " +
+                $"Sum of x and y is: {s2}");
+            Console.ReadKey();
+            Console.WriteLine("\n..........................................................................");
+
+//...........................................................................
